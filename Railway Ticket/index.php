0ab114a68a251b7e6c62fc1bea8fde254ed4c3e5
Mostafiz222 <mostafiz124512@gmail.com>
2025-11-20 18:09:15 +0600

    Add files via upload

---
+++ Railway Ticket/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?v=2.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="nav">
+        <a href="index.php">
+            <button class="aa">Home</button>
+        </a>
+        <a href="Login.php">
+            <button class="aa">Login</button>
+        </a>
+        <a href="regester.php">
+            <button class="aa">Regester</button>
+        </a>
+        <a href="train.php">
+            <button class="aa">Train info</button>
+        </a>
+    </nav>
+    <div class="wel">
+        <h1>Welcome to online train ticket booking system.</h1>
+    </div>
+    <div class="a">
+        <img  class="img" src="" alt="Description of the image" >
+        <h3>This is a demo project.Other functionality works fine.</h3>
+    </div>
+    <script src="script.js"></script> 
+</body>
+</html>
